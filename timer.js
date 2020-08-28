0da97803bcc76045b1b04201401b68878f37ba04
LiamN69 <69023006+LiamN69@users.noreply.github.com>
2020-08-28 21:47:07 +1000

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,13 +1,23 @@
 window.onload=function() {
-    secObj = document.getElementById('showscs');
-    minObj = document.getElementById('showmns');
+	minObj = document.getElementById('mns');
+    secObj = document.getElementById('scs');
     document.getElementById('btnct').onclick = countdownTimer;
 }
 
 function countdownTimer() {
+	var min = new Number(minObj.value);
     var sec = new Number(secObj.value);
-    var min = new Number(minObj.value);
-	
-document.getElementById("showmns").innerHTML = min;
+ 	
+	document.getElementById("showmns").innerHTML = min;
+if (sec > 9) {
+	document.getElementById("showscs").innerHTML = sec;
+}	
+else {
+	document.getElementById("showscs").innerHTML = '0' + sec;
+}
+if (sec > 59) {
+    alert("Please input a value  0-59");
+    document.getElementById("showscs").innerHTML = "00";
+}
 }
 
